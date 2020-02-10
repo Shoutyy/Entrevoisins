4f682a3384387923d51d12678ed7f957d36d21f7
Benjamin Debray <shouty91600@hotmail.fr>
2020-02-10 14:14:09 +0100

    backbutton

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/DetailNeighbourActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/DetailNeighbourActivity.java
@@ -1,15 +1,29 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.openclassrooms.entrevoisins.R;
 
 public class DetailNeighbourActivity extends AppCompatActivity {
 
+    private ImageButton mBackButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_neighbour);
+        mBackButton = findViewById(R.id.backButton);
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DetailNeighbourActivity.this, ListNeighbourActivity.class));
+                finish();
+            }
+        });
     }
 }
