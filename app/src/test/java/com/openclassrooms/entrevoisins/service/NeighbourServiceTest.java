c48c2af1413ede30be2f2166d5d3254a41536ba6
Benjamin Debray <shouty91600@hotmail.fr>
2020-03-27 14:03:10 +0100

    update

--- app/src/test/java/com/openclassrooms/entrevoisins/service/NeighbourServiceTest.java
+++ app/src/test/java/com/openclassrooms/entrevoisins/service/NeighbourServiceTest.java
@@ -2,6 +2,7 @@ package com.openclassrooms.entrevoisins.service;
 
 import com.openclassrooms.entrevoisins.di.DI;
 import com.openclassrooms.entrevoisins.model.Neighbour;
+import com.openclassrooms.entrevoisins.ui.neighbour_list.NeighbourFragment;
 
 import org.hamcrest.collection.IsIterableContainingInAnyOrder;
 import org.junit.Before;
@@ -13,6 +14,7 @@ import java.util.List;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test on Neighbour service
@@ -21,6 +23,7 @@ import static org.junit.Assert.assertThat;
 public class NeighbourServiceTest {
 
     private NeighbourApiService service;
+    private NeighbourFragment neighbourList;
 
     @Before
     public void setup() {
